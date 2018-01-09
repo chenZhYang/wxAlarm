17fa9fd185a57e4b0c5958898a50a5e58ed91af6
czy <384736417@qq.com>
2018-01-09 11:14:18 +0800

    增加POST请求功能

--- alarm/urls.py
+++ alarm/urls.py
@@ -6,5 +6,7 @@ from. import views
 app_name ='alarm'
 urlpatterns = [
     url(r'^sendMsg/$', views.sendMsg),
+    url(r'^sendMsgPost$', views.sendMsgPost),
     url(r'^login$', views.login),
+    url(r'^index/$', views.index),
 ]
--- alarm/views.py
+++ alarm/views.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding=utf-8
+from django.shortcuts import render
 import json
-import itchat
+import itchat,sys,time
 from datetime import datetime
 import requests
 from django.views.decorators.csrf import csrf_exempt
@@ -11,15 +12,29 @@ from django.http import HttpRequest, HttpResponse
 # 默认开启了csrf保护机制，本服务仅作自测使用，加上csrf_exempt去除掉csrf保护
 @csrf_exempt
 def sendMsg(request):
-    print('get into fake_query')
+    print('get into sendMsg')
+    print(request.GET)
+    print(request.body)
+    print(request.POST)
     print(json.dumps(request.GET))
     json_str = json.dumps(request.GET)
     json_to_python = json.loads(json_str)
     SentChatRoomsMsg(json_to_python['name'], json_to_python['context'])
     return HttpResponse('OK')
+@csrf_exempt
+def sendMsgPost(request):
+    if 'POST' == request.method:
+        print('get into sendMsgPost')
+        #print(json.loads(request.POST))
+        print(request.body)
+        print(json.loads(request.body))
+        #json_str = json.dumps(request.body)
+        json_to_python = json.loads(request.body)
+        SentChatRoomsMsg(json_to_python['name'], json_to_python['context'])
+    return HttpResponse('OK')
 
 def login(request):
-    itchat.auto_login(hotReload=True)
+    itchat.auto_login(hotReload=2)
     itchat.run()
     return
 
@@ -35,3 +50,6 @@ def SentChatRoomsMsg(name,context):
                                                                    "发送到：" + name + "\n"
                                                                                    "发送内容：" + context + "\n")
     print("*********************************************************************************")
+
+def index(request):
+    return render(request, "alarm/index.html")
